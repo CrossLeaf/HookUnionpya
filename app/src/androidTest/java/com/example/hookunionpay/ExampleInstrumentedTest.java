cddc9d65d9d1bf9aa2e9d8af055a23f25664df7d
Eton <eton_tsai@paradise-soft.com.tw>
2019-02-22 11:33:07 +0800

    test

--- app/src/androidTest/java/com/example/hookunionpay/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/hookunionpay/ExampleInstrumentedTest.java
@@ -1,8 +1,12 @@
 package com.example.hookunionpay;
 
+import android.app.Instrumentation;
 import android.content.Context;
+import android.content.Intent;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.runner.AndroidJUnit4;
+
+import junit.framework.TestCase;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -15,12 +19,21 @@ import static org.junit.Assert.*;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 @RunWith(AndroidJUnit4.class)
-public class ExampleInstrumentedTest {
+public class ExampleInstrumentedTest extends TestCase {
     @Test
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
         assertEquals("com.example.hookunionpay", appContext.getPackageName());
+    }
+
+    @Test
+    public void testA(){
+        Instrumentation instrumentation = InstrumentationRegistry.getInstrumentation();
+        Context context = instrumentation.getContext();               // 启动测试App
+        Intent intent = context.getPackageManager().getLaunchIntentForPackage("com.unionpay");
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        context.startActivity(intent);
     }
 }
--- app/src/main/java/com/example/hookunionpay/MainActivity.java
+++ app/src/main/java/com/example/hookunionpay/MainActivity.java
@@ -1,5 +1,8 @@
 package com.example.hookunionpay;
 
+import android.app.Instrumentation;
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +12,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void testA(){
+//        Instrumentation instrumentation = InstrumentationRegistry.getInstrumentation();
+//        Context context = instrumentation.getContext();               // 启动测试App
+//        Intent intent = context.getPackageManager().getLaunchIntentForPackage("com.unionpay");
+//        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+//        context.startActivity(intent);
+
     }
 }
